04e8670af985d12e899eb48e2af41b6865139ec0
danielle34 <danie3m3@gmail.com>
2024-01-22 18:43:03 -0500

    added folders that u can click on to open a window. picked a radnom folder color but want a retro look, next is the bottom part

--- move_box.js
+++ move_box.js
@@ -24,7 +24,7 @@ function move(ele_clicked) {
     // console.log("ufihu3rhuif3f",windows)
 
     windows.forEach(other_window => {
-        console.log("parenttt ",other_window.parentNode)
+        console.log("parenttt ", other_window.parentNode)
         other_window.parentNode.style.zIndex = "1";
         // console.log("ummmmmmmmmmemefmefer",other_window.className,other_window.style.zIndex);
     });
@@ -36,31 +36,31 @@ function move(ele_clicked) {
     windows.forEach(each_window => {
         const window_mover = each_window.getElementsByClassName("top_box")[0].getElementsByClassName("top_click_box")[0]
         window_mover.addEventListener("mousedown", () => {
-            
+
 
 
             // each_window.parentNode.style.position = "absolute"
 
             document.onmousemove = (e) => {
                 //const = each_window.parentNode.style.left
-                
-                //const has_to_be = true;
-                if (each_window!=null ){
-                    
 
-                    var has_to_be = parseInt(each_window.parentNode.style.left.substring(0,each_window.parentNode.style.length));
-                    var filler = has_to_be>0;
-                    console.log("has ",typeof has_to_be, has_to_be, filler)
-                    if(true){// idk why this isn;rt workinh
+                //const has_to_be = true;
+                if (each_window != null) {
+
+
+                    var has_to_be = parseInt(each_window.parentNode.style.left.substring(0, each_window.parentNode.style.length));
+                    var filler = has_to_be > 0;
+                    console.log("has ", typeof has_to_be, has_to_be, filler)
+                    if (true) {// idk why this isn;rt workinh
                         const mouseX = e.movementX;
                         const mouseY = e.movementY;
                         each_window.parentNode.style.left = each_window.parentNode.offsetLeft + mouseX + 'px';
                         each_window.parentNode.style.top = each_window.parentNode.offsetTop + mouseY + 'px';
-                        console.log(mouseX,mouseY, each_window.parentNode.style.left, each_window.parentNode.style.top)
-                        console.log(parseInt(each_window.parentNode.style.left.substring(0,each_window.parentNode.style.length)))
-    
+                        console.log(mouseX, mouseY, each_window.parentNode.style.left, each_window.parentNode.style.top)
+                        console.log(parseInt(each_window.parentNode.style.left.substring(0, each_window.parentNode.style.length)))
+
                     }
-                   
+
                 }
 
 
@@ -73,6 +73,6 @@ function move(ele_clicked) {
             }
         })
 
-        
+
     });
 }
