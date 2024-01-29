3a130c65885c944740c155c58e22dcf1fe8b9db4
Mikito-Coder <Ayandosu10@gmail.com>
2024-01-29 12:44:41 -0500

    Ofiicial changes to the FAQ page. I commented the JS ands CSS files. For now evrything is in FAQ.html

--- FAQ.js
+++ FAQ.js
@@ -1,26 +1,26 @@
 
 
-function add_text(the_button){
-    var the_question = the_button.parentNode;
-    console.log("yooooo",the_question);
-    var extra_stuff = the_question.getElementsByClassName("extra_text")[0];
-    extra_stuff.style.display = 'block';
-    the_button.style.display = 'none';
-    console.log("adededed texttt",the_button);
-    var remove_button = the_question.getElementsByClassName("remove_button")[0];
-    remove_button.style.display = 'block';
-}   
+// function add_text(the_button){
+//     var the_question = the_button.parentNode;
+//     console.log("yooooo",the_question);
+//     var extra_stuff = the_question.getElementsByClassName("extra_text")[0];
+//     extra_stuff.style.display = 'block';
+//     the_button.style.display = 'none';
+//     console.log("adededed texttt",the_button);
+//     var remove_button = the_question.getElementsByClassName("remove_button")[0];
+//     remove_button.style.display = 'block';
+// }   
 
 
-function remove_text(the_button){
-    var the_question = the_button.parentNode;
-    console.log("yooooo",the_question);
-    var extra_stuff = the_question.getElementsByClassName("extra_text")[0];
-    extra_stuff.style.display = 'none';
-    the_button.style.display = 'none';
-    console.log("adededed texttt",the_button);
-    var remove_button = the_question.getElementsByClassName("add_button")[0];
-    remove_button.style.display = 'block';
-}
+// function remove_text(the_button){
+//     var the_question = the_button.parentNode;
+//     console.log("yooooo",the_question);
+//     var extra_stuff = the_question.getElementsByClassName("extra_text")[0];
+//     extra_stuff.style.display = 'none';
+//     the_button.style.display = 'none';
+//     console.log("adededed texttt",the_button);
+//     var remove_button = the_question.getElementsByClassName("add_button")[0];
+//     remove_button.style.display = 'block';
+// }
 
-// getElementsByClassName("top_box")[0]
+// // getElementsByClassName("top_box")[0]
