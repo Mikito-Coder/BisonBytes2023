2cc3d51f6997300a8485ea4cae3b4e2344d98970
danielle34 <danie3m3@gmail.com>
2024-02-20 07:57:09 -0500

    adding functios for moving things in the background updating main site too

--- background_img_f.js
+++ background_img_f.js
@@ -1,3 +1,8 @@
+
+window.onload = function () {
+
+
+}
 function timer() {
     disp();
     my_time = setTimeout('timer()', 10);
@@ -24,4 +29,19 @@ function disp() {
             x = x + step; document.getElementById('i1').style.left = x + "px";//horizontal move
         }
     }
+}
+
+function move_cloud(ele_clicked){
+    var button_id = ele_clicked.id;
+    for(var i=0;i<5;i++) {
+        setTimeout(function(ele_clicked) {
+           
+
+            each_window.style.left = each_window.parentNode.offsetLeft + 1 + 'px';
+            each_window.style.top = each_window.parentNode.offsetTop + 1 + 'px';
+
+            
+        },(i+i+1)*1000);
+    }
+
 }
